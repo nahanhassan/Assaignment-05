913030ab25c3f3fbf722113866eef5496c2b1514
unknown <sweetcode.js@gmail.com>
2024-09-24 06:07:44 +0600

    sticky part is done

--- app.js
+++ app.js
@@ -9,6 +9,28 @@ function getTextById(id) {
 }
 
 let navDisplay = document.getElementById("navDisplay");
+let mainDonation = document.getElementById("mainDonation");
+let mainHistory = document.getElementById("mainHistory");
+let donationHistory = document.getElementById("donationHistory");
+let transitionHistory = document.getElementById("transitionHistory");
+
+mainHistory.addEventListener("click", function () {
+  mainDonation.classList.remove("btn-2");
+  mainDonation.classList.add("btn-3");
+  mainHistory.classList.remove("btn-3");
+  mainHistory.classList.add("btn-2");
+  transitionHistory.classList.remove("hidden");
+  donationHistory.classList.add("hidden");
+});
+
+mainDonation.addEventListener("click", function () {
+  mainDonation.classList.remove("btn-3");
+  mainDonation.classList.add("btn-2");
+  mainHistory.classList.remove("btn-2");
+  mainHistory.classList.add("btn-3");
+  donationHistory.classList.remove("hidden");
+  transitionHistory.classList.add("hidden");
+});
 
 document
   .getElementById("noakhaliDonate")
@@ -29,10 +51,10 @@ document
       alert("Please check your amount");
       const div = document.createElement("div");
       div.innerHTML = `
-
+    <div class="bg-slate-50 mt-1 mx-auto w-5/6">
       <h2 class="text-xl" > Invalid Transition</h2>
       <p>Date: ${new Date()}</p>
-
+    </div>
       `;
       document.getElementById("transitionHistory").appendChild(div);
     } else {
@@ -45,11 +67,100 @@ document
 
       const div = document.createElement("div");
       div.innerHTML = `
-
-      <h4 class="text-xl" > ${noakhaliAmount} Taka is Donated for Flood Relief in Noakhali ,Bangladesh</h4>
-      <p>Date: ${new Date()}</p>
+      <div class="bg-slate-50 mt-1 mx-auto w-5/6">
+        <h4 class="text-xl" > ${noakhaliAmount} Taka is Donated for Flood Relief in Noakhali ,Bangladesh</h4>
+        <p>Date: ${new Date()}</p>
+      </div>
 
       `;
       document.getElementById("transitionHistory").appendChild(div);
     }
   });
+
+// dhaka
+
+document.getElementById("dhakaDonate").addEventListener("click", function (e) {
+  e.preventDefault();
+  let dhakaDisplay = document.getElementById("dhakaDisplay");
+  let dhakaDonate = document.getElementById("dhakaAmount");
+  let dhakaAmount = getValueById("dhakaAmount");
+  let dhakaDisplayAmount = getTextById("dhakaDisplay");
+  let navDisplayAmount = getTextById("navDisplay");
+
+  if (
+    navDisplay <= 0 ||
+    dhakaAmount <= 0 ||
+    dhakaAmount > navDisplayAmount ||
+    isNaN(dhakaAmount) === true
+  ) {
+    alert("Please check your amount");
+    const div = document.createElement("div");
+    div.innerHTML = `
+
+      <h2 class="text-xl" > Invalid Transition</h2>
+      <p>Date: ${new Date()}</p>
+
+      `;
+    document.getElementById("transitionHistory").appendChild(div);
+  } else {
+    let dhakaNewAmount = dhakaAmount + dhakaDisplayAmount;
+    let navNewAmount = navDisplayAmount - dhakaAmount;
+
+    dhakaDisplay.innerText = dhakaNewAmount;
+    dhakaDonate.value = "";
+    navDisplay.innerText = navNewAmount;
+
+    const div = document.createElement("div");
+    div.innerHTML = `
+
+      <h4 class="text-xl" > ${dhakaAmount} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h4>
+      <p>Date: ${new Date()}</p>
+
+      `;
+    document.getElementById("transitionHistory").appendChild(div);
+  }
+});
+
+//feni
+
+document.getElementById("feniDonate").addEventListener("click", function (e) {
+  e.preventDefault();
+  let feniDisplay = document.getElementById("feniDisplay");
+  let feniDonate = document.getElementById("feniAmount");
+  let feniAmount = getValueById("feniAmount");
+  let feniDisplayAmount = getTextById("feniDisplay");
+  let navDisplayAmount = getTextById("navDisplay");
+
+  if (
+    navDisplay <= 0 ||
+    feniAmount <= 0 ||
+    feniAmount > navDisplayAmount ||
+    isNaN(feniAmount) === true
+  ) {
+    alert("Please check your amount");
+    const div = document.createElement("div");
+    div.innerHTML = `
+
+      <h2 class="text-xl" > Invalid Transition</h2>
+      <p>Date: ${new Date()}</p>
+
+      `;
+    document.getElementById("transitionHistory").appendChild(div);
+  } else {
+    let feniNewAmount = feniAmount + feniDisplayAmount;
+    let navNewAmount = navDisplayAmount - feniAmount;
+
+    feniDisplay.innerText = feniNewAmount;
+    feniDonate.value = "";
+    navDisplay.innerText = navNewAmount;
+
+    const div = document.createElement("div");
+    div.innerHTML = `
+
+      <h4 class="text-xl" > ${feniAmount} Taka is Donated for Flood Relief in Feni ,Bangladesh</h4>
+      <p>Date: ${new Date()}</p>
+
+      `;
+    document.getElementById("transitionHistory").appendChild(div);
+  }
+});
